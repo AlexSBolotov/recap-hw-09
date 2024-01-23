5c3e4d988aca7f046f9d5e0ff03cffe4c46886fa
AlexSBolotov <122620169+AlexSBolotov@users.noreply.github.com>
2024-01-23 17:23:07 +0000

    final

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,23 +1,15 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 const form = document.querySelector('.form');
 
-const onFormSubmit = e => {
-  e.preventDefault();
-  const { delay, step, amount } = e.target.elements;
-  let interval = Number(delay.value);
-
-  for (let i = 1; i <= amount.value; i++) {
-    createPromise(i, interval)
-      .then(({ position, delay }) =>
-        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`)
-      )
-      .catch(({ position, delay }) =>
-        console.log(`❌ Rejected promise ${position} in ${delay}ms`)
-      );
-    interval += Number(step.value);
-  }
-};
-
 const createPromise = (position, delay) => {
+  // setTimeout(() => {
+  //   const shouldResolve = Math.random() > 0.3;
+  //   if (shouldResolve) {
+  //     return Promise.resolve({ position, delay });
+  //   }
+  //   return Promise.reject({ position, delay });
+  // }, delay);
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       const shouldResolve = Math.random() > 0.3;
@@ -29,6 +21,24 @@ const createPromise = (position, delay) => {
     }, delay);
   });
 };
+
+const onFormSubmit = e => {
+  e.preventDefault();
+  const { delay, step, amount } = e.target.elements;
+  let interval = Number(delay.value);
+
+  for (let i = 1; i <= amount.value; i++) {
+    createPromise(i, interval)
+      .then(({ position, delay }) =>
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+      )
+      .catch(({ position, delay }) =>
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+      );
+    interval += Number(step.value);
+  }
+};
+
 form.addEventListener('submit', onFormSubmit);
 
 // const fetchUserFromServer = username => {
